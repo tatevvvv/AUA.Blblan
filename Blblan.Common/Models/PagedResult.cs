48b2bc13f99d0468fa826aeb23ecaa9d5d615b19
bmbul <smbat.eghiazaryan@gmail.com>
2024-05-01 12:40:18 +0400

    updated ignore file

---
+++ Blblan.Common/Models/PagedResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Blblan.Common.Models;
+
+public class PagedResult<T>
+{
+    public List<T> Items { get; set; }
+
+    public int TotalCount { get; set; }
+
+    public PagedResult()
+    {
+        Items = new List<T>();
+        TotalCount = 0;
+    }
+}
