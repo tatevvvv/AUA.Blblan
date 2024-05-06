b0d1a9b0a8d2f404a923795ba04b96ab8c27d54e
Tatevik Abrahamyan <tabrahamyan@zerosystems.com>
2024-05-06 10:16:24 +0400

    business layer added

---
+++ Blblan.BusinessLayer/ConversationService.cs
@@ -0,0 +1,84 @@
+﻿using Blblan.Common.Models;
+using Blblan.Common.Services;
+using Blblan.Data.Entities;
+using Blblan.Data.Repositories;
+using PredictionClientApp;
+
+namespace Blblan.BusinessLayer
+{
+    internal class ConversationService : IConversationService
+    {
+        private readonly PredictionEngineClient _predictionEngineClient;
+        private readonly ConversationRepository _conversationRepository;
+        private readonly MessagesRepository _messagesRepository;
+        private readonly UserRepository _userRepository;
+
+        public ConversationService(
+            PredictionEngineClient predictionEngineClient,
+            ConversationRepository conversationRepository,
+            MessagesRepository messagesRepository,
+            UserRepository userRepository)
+        {
+            _predictionEngineClient = predictionEngineClient;
+            _conversationRepository = conversationRepository;
+            _messagesRepository = messagesRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<AnswerModel> SendMessageAsync(int userId, QuestionModel messageModel)
+        {
+            var conversation = await _conversationRepository.GetByIdAsync(messageModel.contextId).ConfigureAwait(false);
+
+            if (conversation == null)
+            {
+                throw new Exception($"Conversation not found error for Id : {messageModel.contextId}");
+            }
+
+            try
+            {
+                // collecting context by conversationID
+                var allMessages = _messagesRepository.Set().Where(m => m.ConversationId == conversation.Id).ToList();
+
+                var response = await _predictionEngineClient.MakePredictionAsync(messageModel).ConfigureAwait(false);
+                var result = new AnswerModel(response, messageModel.contextId);
+
+                // after getting the response from prediction engine store it in db.
+
+                conversation.Messages.Add(new Message
+                {
+                    Question = messageModel.content,
+                    Answer = response,
+                    ConversationId = messageModel.contextId,
+                    Conversation = conversation,
+                    Timestamp = DateTime.UtcNow,
+                });
+
+                return result;
+            }
+            catch (Exception _)
+            {
+                return new AnswerModel(string.Empty, messageModel.contextId);
+            }
+        }
+
+        public async Task<ConversationModel> CreateNewConversation(int userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId).ConfigureAwait(false);
+
+            if (user == null)
+            {
+                throw new Exception($"User not found for {userId}");
+            }
+
+            var conversation = new Conversation
+            {
+                UserId = userId,
+                CreatedAt = DateTime.UtcNow,
+            };
+
+            await _conversationRepository.AddAsync(conversation).ConfigureAwait(false);
+
+            return new ConversationModel(conversation.Id, conversation.Name);
+        }
+    }
+}
---
+++ Blblan.BusinessLayer/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Blblan.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blblan.BusinessLayer
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterDataLayer(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+
+            services.AddDbContext<BlblanDbContext>(options =>
+                options.UseMySql(configuration.GetConnectionString("DefaultConnection"),
+                ServerVersion.AutoDetect(configuration.GetConnectionString("DefaultConnection"))));
+
+            return services;
+        }
+    }
+}
